b33bb269c723ca095b58fb804aad2e1be3f1bb77
Connor Olson <121678858+cbolson22@users.noreply.github.com>
2026-04-25 14:38:19 -0500

    Home page and Devise Auth Implemented

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
@@ -65,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.expect(post: [ :title, :description, :meal_date, :user_id ])
+      params.expect(post: [ :title, :description, :meal_date ])
     end
 end
