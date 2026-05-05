bfea42b76ac63ecb0d8b10183bc989125b928abc
Connor Olson <121678858+cbolson22@users.noreply.github.com>
2026-05-05 13:37:00 -0500

    Enforce unique usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+
   has_many :posts, -> { order(meal_date: :asc) }, dependent: :destroy
   has_many :rsvps, dependent: :destroy
 
