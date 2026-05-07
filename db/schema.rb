b457fbb749d00b6cba89cbf08cd43ff951df0798
Nick Perry <nperry248@gmail.com>
2026-05-07 13:38:13 -0500

    unsplash auto photo retrieve

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_06_233613) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_07_000256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_06_233613) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string "cover_photo_url"
     t.datetime "created_at", null: false
     t.text "description"
     t.datetime "meal_date"
