79c1688f246ca19e4862a5955c803198d1c86b26
calebweldon <caleb.weldon@atscale.com>
2026-05-13 16:50:53 -0500

    moved dietary restrictions helper into its own file out of application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,4 @@
 module ApplicationHelper
-  DIETARY_RESTRICTION_OPTIONS = {
-    "vegetarian"     => "Vegetarian",
-    "vegan"          => "Vegan",
-    "gluten_free"    => "Gluten-Free",
-    "dairy_free"     => "Dairy-Free",
-    "nut_free"       => "Nut-Free",
-    "shellfish_free" => "Shellfish-Free",
-    "halal"          => "Halal",
-    "kosher"         => "Kosher"
-  }.freeze
-
-  def dietary_restriction_label(key)
-    DIETARY_RESTRICTION_OPTIONS[key] || key.to_s.humanize
-  end
-
-  def unmet_dietary_restrictions(post)
-    return [] unless user_signed_in? && current_user.dietary_restrictions.present?
-    current_user.dietary_restrictions - (post.dietary_restrictions || [])
-  end
-
   def avatar_tag(user, size: :md)
     css = "avatar avatar-#{size}"
     if user.avatar.attached?
---
+++ app/helpers/dietary_restrictions_helper.rb
@@ -0,0 +1,21 @@
+module DietaryRestrictionsHelper
+  DIETARY_RESTRICTION_OPTIONS = {
+    "vegetarian"     => "Vegetarian",
+    "vegan"          => "Vegan",
+    "gluten_free"    => "Gluten-Free",
+    "dairy_free"     => "Dairy-Free",
+    "nut_free"       => "Nut-Free",
+    "shellfish_free" => "Shellfish-Free",
+    "halal"          => "Halal",
+    "kosher"         => "Kosher"
+  }.freeze
+
+  def dietary_restriction_label(key)
+    DIETARY_RESTRICTION_OPTIONS[key] || key.to_s.humanize
+  end
+
+  def unmet_dietary_restrictions(post)
+    return [] unless user_signed_in? && current_user.dietary_restrictions.present?
+    current_user.dietary_restrictions - (post.dietary_restrictions || [])
+  end
+end
