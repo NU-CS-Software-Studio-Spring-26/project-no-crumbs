7c23b23a6a81464b8abc2ddc077cc904a0b00ab5
Nick Perry <nperry248@gmail.com>
2026-05-17 20:46:20 -0500

    Wire omniauth routes and configure Google, GitHub, Discord providers in Devise initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: {
+    registrations: "registrations",
+    omniauth_callbacks: "omniauth_callbacks"
+  }
   resources :posts do
     resource :rsvp, only: %i[create destroy]
     resource :like, only: %i[create]
