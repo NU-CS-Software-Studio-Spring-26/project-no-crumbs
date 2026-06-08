c5b98a50d03da4c58b246a526b4322935b60192b
calebweldon <caleb.weldon@atscale.com>
2026-06-07 20:44:41 -0500

    add a community guidelines page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[about legal]
+  skip_before_action :authenticate_user!, only: %i[about legal community_guidelines]
 
   def about; end
   def legal; end
+  def community_guidelines; end
 end
